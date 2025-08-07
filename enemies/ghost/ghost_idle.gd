2fdbd832edaa006f6e26c6360be940af326f2af3
braticpetar <braticpetar44@proton.me>
2025-08-07 17:55:48 +0200

    ghosts are now slowly moving towards player when idle

--- enemies/ghost/ghost_idle.gd
+++ enemies/ghost/ghost_idle.gd
@@ -2,10 +2,13 @@ class_name GhostIdleState extends GhostState
 
 static var state_name = "GhostIdleState"
 
-const STOP_FORCE: float = 15.0
+const SPEED: float = 20
 
 func enter() -> void:
-	ghost.velocity = Vector2(0, 0)
+	ghost.velocity = Vector2.ZERO
+	
+func exit() -> void:
+	ghost.velocity = Vector2.ZERO
 
 func get_state_name() -> String:
 	return state_name
@@ -14,6 +17,11 @@ func process(_delta: float) -> void:
 	animation.play("idle")
 	ghost.handle_facing()
 	
-#func physics_process(_delta: float) -> void:
-	#if ghost.horizontal_input != 0.0:
-		#state_machine.transition(PlayerRunningState.state_name)
+func physics_process(_delta: float) -> void:
+	var direction = (PlayerSingleton.player.get_global_position() - ghost.global_position).normalized()
+	ghost.velocity.x  = direction.x * SPEED
+	ghost.move_and_slide()
+	
+
+		
+		
