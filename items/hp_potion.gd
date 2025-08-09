9ba81fa862f27c188ba76dbc3fd70c32daa00f3d
braticpetar <braticpetar44@proton.me>
2025-08-09 16:05:49 +0200

    implemented potion animation

--- items/hp_potion.gd
+++ items/hp_potion.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+func _ready() -> void:
+	$AnimationPlayer.get_animation("default").loop = true
+	$AnimationPlayer.play("default")
+
 # Be consumed, functions the same as damage, just does negative damage
 func _on_custom_hit_box_area_entered(area: Area2D) -> void:
 	queue_free()
